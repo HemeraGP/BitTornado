7d015e50db73cf0e9c68724d73a9fb2b89c030c3
Christopher J. Johnson <effigies@gmail.com>
2013-05-29 00:55:18 -0400

    RF Update btreannounce and btshowmeta to Python 3; propagate necessary updates

--- BitTornado/Application/reannounce.py
+++ BitTornado/Application/reannounce.py
@@ -6,15 +6,15 @@ def reannounce(fname, announce, announce_list=None, verbose=False):
     metainfo = MetaInfo.read(fname)
 
     if verbose:
-        print 'old announce for %s: %s' % (fname, metainfo['announce'])
+        print('old announce for {}: {}'.format(fname, metainfo['announce']))
 
     metainfo['announce'] = announce
 
     if 'announce-list' in metainfo:
         if verbose:
-            print 'old announce-list for {}: {}'.format(
+            print('old announce-list for {}: {}'.format(
                 fname, '|'.join(','.join(tier)
-                                for tier in metainfo['announce-list']))
+                                for tier in metainfo['announce-list'])))
         if announce_list is not None:
             metainfo['announce-list'] = announce_list
         else:
